2da3d202b76f0fe2624101b2d0d8dc97d70afdc3
lucijahajdu <lucija.hajdu@gmail.com>
2023-09-07 17:04:08 +0000

    Allow logged-in users to book a table

--- booking/views.py
+++ booking/views.py
@@ -19,9 +19,35 @@ class PostDetail(View):
 
         return render(
             request,
-            'booking/booking_list.html',
+            'bookings/booking_list.html',
             {
                 'bookings': bookings,
+                'booked' : False,
+                'booking_form': BookingForm()
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+        bookings = Booking.objects.all()
+        booking_form = BookingForm(data=request.POST)
+
+        if booking_form.is_valid():
+            booking_form.instance.email = request.user.email
+            booking_form.instance.name = request.user.username
+            booking_form.instance.user = request.user.id
+            booking = booking_form.save(commit=False)
+            booking.save()
+        else:
+            booking_form = BookingForm()
+
+        
+
+        return render(
+            request,
+            'bookings/booking_list.html',
+            {
+                'bookings': bookings,
+                'booked': True,
                 'booking_form': BookingForm()
             },
         )
