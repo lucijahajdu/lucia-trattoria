daa83ee702e5a2a1fd6353d8c3d03a55f7ee47e2
lucijahajdu <lucija.hajdu@gmail.com>
2023-09-08 20:00:34 +0000

    Style footer and adjust home page

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import HomeView, PostDetail, EditBooking, DeleteBooking
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('bookings/<int:pk>/', EditBooking.as_view(), name='edit-booking'),
     path('bookings/delete/<int:pk>/', DeleteBooking.as_view(), name='delete-booking'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
