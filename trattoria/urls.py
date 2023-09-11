746d5611a8f43e8192c9a91b8963327f3f96be7b
lucijahajdu <lucija.hajdu@gmail.com>
2023-09-11 15:24:24 +0000

    Fix deployment

--- trattoria/urls.py
+++ trattoria/urls.py
@@ -20,6 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('booking/', include('booking.urls')),
+    path('', include('booking.urls')),
     path('accounts/', include('allauth.urls')),
 ]
